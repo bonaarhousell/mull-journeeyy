863d00d3e61203afd39af5eb1d64e5dfc301fe39
bonaarhousell <nurmuliadi024@gmail.com>
2026-04-08 18:20:18 +0700

    07/06/26 problem solving day 11

---
+++ second_largest.py
@@ -0,0 +1,40 @@
+#breakdown:
+#   input list of number
+#   output: nilai kedua terbesar
+#   objectiv: mencari nilai kedua terbesar
+
+#pseudocode:
+#   buat function
+#   buat edge case tidak boleh list < 2
+#   buat 2 variabel pembanding
+#   loop list number
+#   jika  variabel 1 < parameter loop
+#   maka update nilai variabel 2 menjadi variabel 1
+#   dan variabel 1 menjadi value parameter loop
+#   elif variabel 2 < parameter loop
+#   maka update variabel 2 menjadi parameter loop
+
+#code:
+numbers = [2,3,5,7,8,9,10]
+def second_largest(number):
+    if not number:
+        return "List cannot empty"
+    if len(number) < 2:
+        return "List must have at least two elements"
+    largest = None
+    second_largest = None
+    for num in number:
+        if largest is None:
+            largest = num
+        elif largest < num:
+            second_largest = largest
+            largest = num
+        elif second_largest < num:
+            second_largest = num
+
+    return second_largest
+
+print(second_largest(numbers))
+
+#test_result:
+#   one try
